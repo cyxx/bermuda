86cafe4e78c41c6159d6ea143e8963c2c35a10eb
Gregory Montoir <cyx@users.sourceforge.net>
2011-02-22 16:54:24 +0100

    use stdint types

--- intern.h
+++ intern.h
@@ -11,13 +11,14 @@
 #include <cstdlib>
 #include <cstring>
 #include <cerrno>
+#include <stdint.h>
 
-typedef unsigned char uint8;
-typedef signed char int8;
-typedef unsigned short uint16;
-typedef signed short int16;
-typedef unsigned int uint32;
-typedef signed int int32;
+typedef uint8_t uint8;
+typedef int8_t int8;
+typedef uint16_t uint16;
+typedef int16_t int16;
+typedef uint32_t uint32;
+typedef int32_t int32;
 
 #include "util.h"
 
