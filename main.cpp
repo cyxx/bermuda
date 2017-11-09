e2136105e1b78563c7fb7da449f2162c44804ced
Gregory Montoir <cyx@users.sourceforge.net>
2017-11-09 09:14:38 +0800

    use getopt for command line parsing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
  * Copyright (C) 2007-2011 Gregory Montoir
  */
 
+#include <getopt.h>
+#include <sys/stat.h>
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
@@ -16,23 +18,12 @@ static const char *USAGE =
 	"  --savepath=PATH   Path to save files (default '.')\n"
 	"  --musicpath=PATH  Path to music files (default 'MUSIC')\n";
 
-static bool parseOption(const char *arg, const char *longCmd, const char **opt) {
-	bool handled = false;
-	if (arg[0] == '-' && arg[1] == '-') {
-		if (strncmp(arg + 2, longCmd, strlen(longCmd)) == 0) {
-			*opt = arg + 2 + strlen(longCmd);
-			handled = true;
-		}
-	}
-	return handled;
-}
-
 static Game *g_game;
 static SystemStub *g_stub;
 
 static void init(const char *dataPath, const char *savePath, const char *musicPath) {
 	g_stub = SystemStub_SDL_create();
-	g_game = new Game(g_stub, dataPath, savePath, musicPath);
+	g_game = new Game(g_stub, dataPath ? dataPath : "DATA", savePath ? savePath : ".", musicPath ? musicPath : "MUSIC");
 	g_game->init();
 }
 
@@ -53,18 +44,41 @@ static void mainLoop() {
 
 #undef main
 int main(int argc, char *argv[]) {
-	const char *dataPath = "DATA";
-	const char *savePath = ".";
-	const char *musicPath = "MUSIC";
-	for (int i = 1; i < argc; ++i) {
-		bool opt = false;
-		if (strlen(argv[i]) >= 2) {
-			opt |= parseOption(argv[i], "datapath=", &dataPath);
-			opt |= parseOption(argv[i], "savepath=", &savePath);
-			opt |= parseOption(argv[i], "musicpath=", &musicPath);
+	char *dataPath = 0;
+	char *savePath = 0;
+	char *musicPath = 0;
+	if (argc == 2) {
+		// data path as the only command line argument
+		struct stat st;
+		if (stat(argv[1], &st) == 0 && S_ISDIR(st.st_mode)) {
+			dataPath = strdup(argv[1]);
 		}
-		if (!opt) {
-			printf("%s", USAGE);
+	}
+	while (1) {
+		static struct option options[] = {
+			{ "datapath",  required_argument, 0, 'd' },
+			{ "savepath",  required_argument, 0, 's' },
+			{ "musicpath", required_argument, 0, 'm' },
+			{ "help",      no_argument,       0, 'h' },
+			{ 0, 0, 0, 0 }
+		};
+		int index;
+		const int c = getopt_long(argc, argv, "", options, &index);
+		if (c == -1) {
+			break;
+		}
+		switch (c) {
+		case 'd':
+			dataPath = strdup(optarg);
+			break;
+		case 's':
+			savePath = strdup(optarg);
+			break;
+		case 'm':
+			musicPath = strdup(optarg);
+			break;
+		default:
+			fprintf(stdout, "%s", USAGE);
 			return 0;
 		}
 	}
@@ -85,5 +99,8 @@ int main(int argc, char *argv[]) {
 	}
 	fini();
 #endif
+	free(dataPath);
+	free(savePath);
+	free(musicPath);
 	return 0;
 }
